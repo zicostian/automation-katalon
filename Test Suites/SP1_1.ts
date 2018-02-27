af834b9e0aeab62ca4075c9afefd95c12a9051e1
zicostian <agustianzico@gmail.com>
2018-02-27 02:39:23 -0500

    Tes

---
+++ Test Suites/SP1_1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Test case for header component</description>
+   <name>SP1_1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>82fc60a0-8eb1-4db4-9c84-768832c3c1ac</testSuiteGuid>
+</TestSuiteEntity>
